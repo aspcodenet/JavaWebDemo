4f2b23a84e8b7b5653161a03a7852b220d46e020
Stefan Holmberg <stefan.holmberg@systementor.se>
2025-04-25 13:35:29 +0200

    test

--- src/main/java/org/example/javawebdemo/controllers/HomeController.java
+++ src/main/java/org/example/javawebdemo/controllers/HomeController.java
@@ -1,6 +1,8 @@
 package org.example.javawebdemo.controllers;
 
 
+import org.example.javawebdemo.models.HockeyPlayerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,14 +12,14 @@ import java.util.List;
 
 @Controller
 public class HomeController {
+    @Autowired
+    private HockeyPlayerRepository hockeyPlayerRepository;
+
     @GetMapping("/")
     public String home(Model model) {
         // Häömta saker från databas - tex en lista med HpockeyPlayers
-        List<String> players = new ArrayList<>();
-        players.add("Foppa");
-        players.add("Zäta");
-        players.add("Stefan");
-        model.addAttribute("players", players);
+
+        model.addAttribute("players", hockeyPlayerRepository.findAll());
         model.addAttribute("user", "Stefan");
 
         return "home"; // namnet på HTML filen som ska visas
